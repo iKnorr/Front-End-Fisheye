7a5ba7d8d65dda881f23051541d8ca3cdb3a28c9
Ingo Knorr <ingoknorr@gmail.com>
2023-01-07 22:34:50 +0100

    first commit

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,34 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const link = document.createElement("a");
+    link.setAttribute("href", `photographer.html?id=${id}`);
+    const container = document.createElement("div");
+    container.classList.add("photographer_card");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    const cityCountry = document.createElement("h3");
+    cityCountry.textContent = `${city}, ${country}`;
+    const taglineText = document.createElement("p");
+    taglineText.textContent = tagline;
+    const priceText = document.createElement("span");
+    h2.textContent = name;
+    priceText.textContent = `${price}€/jour`;
+    link.appendChild(container);
+    link.appendChild(img);
+    link.appendChild(h2);
+    container.appendChild(img);
+    article.appendChild(link);
+    article.appendChild(cityCountry);
+    article.appendChild(taglineText);
+    article.appendChild(priceText);
+
+    return article;
+  }
+  return { name, picture, id, city, country, tagline, price, getUserCardDOM };
 }
