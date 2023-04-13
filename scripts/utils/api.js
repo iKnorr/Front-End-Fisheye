c5ea7733297a27187093082bb05094dcc60f1280
Ingo Knorr <ingoknorr@gmail.com>
2023-04-13 11:41:45 +0200

    addec ursor to like

--- scripts/utils/api.js
+++ scripts/utils/api.js
@@ -14,7 +14,7 @@ const sortPhotographerMedia = (sortedMedia, currentSortingValue) => {
 };
 
 async function getPhotographers() {
-  const response = await fetch('../../data/photographers.json');
+  const response = await fetch('./data/photographers.json');
   const { photographers, media } = await response.json();
   return {
     photographers,
@@ -23,7 +23,7 @@ async function getPhotographers() {
 }
 
 async function getMedia() {
-  const response = await fetch('../../data/photographers.json');
+  const response = await fetch('./data/photographers.json');
   const { photographers, media } = await response.json();
   const photographer = photographers.filter(i => i.id === idURL).shift();
   const photographerMedia = media.filter(i => i.photographerId === idURL);
